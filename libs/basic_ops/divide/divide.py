29694fa615f309be0014cf98c77ef46aab7714f5
Keyvan Ebrahimpour <k1.ebrahimpour@gmail.com>
2024-12-17 19:02:41 +0100

    remove out of scope folders

--- libs/basic_ops/divide/divide.py
+++
@@ -1,9 +0,0 @@
-"""
-Module for basic division operations.
-"""
-
-def divide(first_number, second_number):
-    """Divide two numbers."""
-    if second_number == 0:
-        raise ValueError("Division by zero is not allowed.")
-    return first_number / second_number
--- libs/basic_ops/multiply/multiply.py
+++
@@ -1,7 +0,0 @@
-"""
-Module for basic multiplication operations.
-"""
-
-def multiply(first_number, second_number):
-    """Multiply two numbers."""
-    return first_number * second_number
