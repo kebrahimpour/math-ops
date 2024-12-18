f90951cd540227e44b5642b28c5074e352a59b31
Keyvan Ebrahimpour <k1.ebrahimpour@gmail.com>
2024-12-18 23:33:04 +0100

    Revert "Merge pull request #6 from zaribeh/feature-division"
    This reverts commit c96e161e366712cc1998e7e581a4caf9ab61ba08, reversing
    changes made to 2b9ed32fefccd2f453b75639b90d8498ec35c43a.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,12 +1,8 @@
-from libs.basic_ops.combine import combine_operations
+"""
+Module: Integration Tests
 
-def test_combine_operations():
-    addition, subtraction, multiplication, division = combine_operations(10, 2, 3, 2)
-    assert addition == 12
-    assert subtraction == 8
-    assert multiplication == 30
-    assert division == 5
-
+This module contains integration tests that verify interactions between
+different components of the project.
 
 ### Tasks for Students:
 1. Write integration tests to verify that the CLI interacts correctly
@@ -24,4 +20,3 @@ def test_addition_subtraction():
     x_add = add(4, 2)
     y_substract = subtract(x_add, 3)
     assert y_substract == 3
- main
